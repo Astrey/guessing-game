98ef75c3f59de1f392103598b8c2a511ab727df2
Anna Glukhova <ann.astrey@gmail.com>
2022-08-13 01:57:13 +0300

    feat: solved

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -2,20 +2,24 @@ class GuessingGame {
     constructor() {}
 
     setRange(min, max) {
-
+        this.min = min;
+        this.max = max;
     }
 
     guess() {
-
+        this.candidate = Math.ceil((this.max + this.min) / 2);
+        console.log(this.candidate+'*');
+        return this.candidate;
     }
 
     lower() {
-
+        this.max = this.candidate;
     }
 
     greater() {
-
+        this.min = this.candidate;
     }
 }
 
 module.exports = GuessingGame;
+
